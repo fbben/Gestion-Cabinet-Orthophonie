61ef17b66acebdb9299c6db1a3ab374a38c39974
Benzii <farestech6@gmail.com>
2024-05-08 22:47:56 +0100

    Edit Orthophonsite Class

--- src/Orthophoniste.java
+++ src/Orthophoniste.java
@@ -1,4 +1,4 @@
-public class Orthophoniste {
+class Orthophoniste {
     private String nom;
     private String Prenom;
     private String adress;
@@ -6,13 +6,17 @@ public class Orthophoniste {
     private String email;
     private String motDePasse;
 
-    public void CreeCompte(String nom, String Prenom, String adress, String nTlph, String email, String motDePasse) {
+    Orthophoniste(String nom, String Prenom, String adress, String nTlph, String email, String motDePasse) {
         this.nom = nom;
         this.Prenom = Prenom;
         this.adress = adress;
         this.nTlph = nTlph;
         this.email = email;
         this.motDePasse = motDePasse;
+    }
+
+    public void CreerCompte() {
+
     }
 
     public void Connecter() {
